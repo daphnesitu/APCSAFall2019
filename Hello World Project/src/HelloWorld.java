9ffdb1f82ce776f26227b30f4fcfc5a012b67612
APCS3rd <APCS3rd@MHS-LT9-24>
2019-08-20 10:12:15 -0700

    complete Hello World Project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,11 @@
+//Daphne Situ
+//8/16/19
+//prints a message "Hello World" to the Console 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.out.println("Hello World!");
+	}
+
+}
